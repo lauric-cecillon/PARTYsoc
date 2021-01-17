1e81c6a0aec7cbc1e39037776834bad13840b2ee
Lauric Cécillon <75581547+lauric-cecillon@users.noreply.github.com>
2021-01-17 16:35:55 +0100

    Update PARTYsocv2.0eu_prediction_Rscript.R

--- PARTYsocv2.0eu_prediction_Rscript.R
+++ PARTYsocv2.0eu_prediction_Rscript.R
@@ -1,29 +1,32 @@
-################################################################################
-#   PARTYsocv2.0eu
-#   A statistical model partitioning soil organic carbon into its
-#   centennially active and stable fractions based on Rock-Eval thermal analysis 
-#   R script by Lauric Cécillon 
-#   Contact: lauric.cecillon@inrae.fr
-################################################################################
+#####################################################################################################
+### R script for running PARTYsocv2.0eu statistical model
+### "A statistical model partitioning soil organic carbon into its
+### centennially active and stable fractions, based on Rock-Eval thermal analysis"
+#####################################################################################################
+### Written by Lauric CÃ©cillon (lauric.cecillon@inrae.fr)
+#####################################################################################################
+### This script accompanies the draft: 
+### "Partitioning soil organic carbon into its centennially active and stable fractions 
+### with statistical models based on Rock-EvalÂ® thermal analysis (PARTYSOCv2.0 and PARTYSOCv2.0EU)"
+### Submitted to Geoscientific Model Development
+### By Lauric CÃ©cillon et al.
+#####################################################################################################
 
 ### Specify pathway for data
-setwd("HERE specify your working directory")
+setwd("set_your_working_directory")
 rm(list=ls())
 
-### ATTENTION!!! Use only on csv file with 18 Rock-Eval parameters
-### ATTENTION: S2 should be in gC_kg 
-#   (from home made R script, not from the Vinci Technologies software where it is in gCH/kg)
-### PC (mgC/g), HI, OI RE6, TOC_RE6 (gC/gkg) are derived from Vinci Technologies software
-#   ordered like this:
+### ATTENTION!!! Use only on csv file with 18 Rock-Eval parameters 
+### calculated as described in the above-mentionned draft
+### Parameters should be named as described below, and ordered accordingly, one variable per column
+# "T70_HC_PYR"	"T90_HC_PYR"	
+# "T30_CO2_PYR"	"T50_CO2_PYR"	"T70_CO2_PYR"	"T90_CO2_PYR"
+# "T70_CO_OX"	
+# "T50_CO2_OX"	"T70_CO2_OX"	"T90_CO2_OX"	
+# "PseudoS1"	"S2"	"S2_PC"	"HI"	"HI_OIre6"	"PC"	"PC_TOCre6"	"TOCre6"
 
-# "t_ch_pyr_70" "t_ch_pyr_90"    
-# "t_co2_pyr_30" "t_co2_pyr_50" "t_co2_pyr_70" "t_co2_pyr_90"
-# "t_co_ox_70" 
-# "t_co2_ox_50" "t_co2_ox_70" "t_co2_ox_90"
-# "pseudos1_gc_kg" "s2_gc_kg" "s2_pc"
-# "hi" "hi_oi_re6" "pc_g_kg" "pc_toc_re6" "toc_re6_g_kg"  
-
-re6_predictors<-read.csv("name_of_file_with_18_Rock-Eval_predictor_variables.csv", 
+# Load csv file with 18 Rock-Eval parameters (predictor variables) of new topsoil samples
+re6_predictors<-read.csv("filename_18_rockeval_predictor_variables.csv", 
                          header=T, sep=";", row.names=1)
 dim(re6_predictors)
 colnames(re6_predictors)
@@ -42,8 +45,8 @@ rownames(new_data_scaled)<-rownames(re6_predictors)
 colnames(new_data_scaled)<-colnames(re6_predictors)
 for (i in 1:ncol(re6_predictors))
 {
-  new_data_scaled[,i]<-(re6_predictors[,i]-attributes(ltbf_cal_scaled_6sitesEU)$"scaled:center"[i])/
-                        attributes(ltbf_cal_scaled_6sitesEU)$"scaled:scale"[i]
+  new_data_scaled[,i]<-(re6_predictors[,i]-attributes(ltbf_cal_scaled)$"scaled:center"[i])/
+    attributes(ltbf_cal_scaled)$"scaled:scale"[i]
 }
 View(new_data_scaled)
 dim(new_data_scaled)
@@ -51,33 +54,34 @@ dim(new_data_scaled)
 ###########################################################################################
 ### Random Forests regression model
 ### Make a prediction of the centennially stable SOC proportion, including error, 
-#  for a new soil sample
-### NEW DATASET FOR RF model (X variables)
+### for a new soil sample
 library(randomForest)
-rf_model_6sitesEU_pred <- predict(rf_model_6sitesEU, new_data_scaled, predict.all=TRUE)
-names(rf_model_6sitesEU_pred)
-rf_model_6sitesEU_pred$aggregate
-dim(rf_model_6sitesEU_pred$individual)
+partysocv2.0eu_pred <- predict(rf_model, new_data_scaled, predict.all=TRUE)
+names(partysocv2.0eu_pred)
+partysocv2.0eu_pred$aggregate
+dim(partysocv2.0eu_pred$individual)
 
-# store standard deviations of predictions for each new sample (over 1000 trees)
+# Store standard deviations of predictions for each new sample (over 1000 trees)
 sd_pred_model<-vector()
-
-for (i in 1:nrow(rf_model_6sitesEU_pred$individual))
+for (i in 1:nrow(partysocv2.0eu_pred$individual))
 {
-  sd_pred_model[i]<-sd(rf_model_6sitesEU_pred$individual[i,])
+  sd_pred_model[i]<-sd(partysocv2.0eu_pred$individual[i,])
 }
-
 sd_pred_model
 
-# store prediction and compute final error for each validation sample
-final_pred_error<-matrix(nrow=nrow(rf_model_6sitesEU_pred$individual), ncol=3,0)
-# Modèle PARTYsocv2.0eu
-colnames(final_pred_error)<-c("CPSOC_proportion_PARTYsocv2.0eu", "CPSOC_proportion_PARTYsocv2.0eu_ci95_mc", "CPSOC_proportion_PARTYsocv2.0eu_ci95_boot")
-
-rownames(final_pred_error)<-names(rf_model_6sitesEU_pred$aggregate)
-final_pred_error[,1]<-rf_model_6sitesEU_pred$aggregate
-final_pred_error[,2]<-sd_pred_model*t_value_MC_6sitesEU
-final_pred_error[,3]<-sd_pred_model*t_value_BOOT_6sitesEU
+### Store PARTYsocv2.0eu predictions of the centennially stable SOC proportion in new topsoils 
+#   and calculate prediction error (confidence interval; ci95) for each sample, using a Monte-Carlo (mc) or a bootstrap (boot) approach
+#   Also calculate the content of the centennially stable SOC fraction, 
+#   and the proportion and content of the centennially active SOC fraction
+final_pred_error<-matrix(nrow=nrow(partysocv2.0eu_pred$individual), ncol=6,0)
+colnames(final_pred_error)<-c("Stable_SOC_proportion_PARTYsocv2.0eu", "Stable_SOC_proportion_PARTYsocv2.0eu_ci95_mc", "Stable_SOC_proportion_PARTYsocv2.0eu_ci95_boot", "Stable_SOC_content_PARTYsocv2.0eu", "Active_SOC_proportion_PARTYsocv2.0eu", "Active_SOC_content_PARTYsocv2.0eu")
+rownames(final_pred_error)<-names(partysocv2.0eu_pred$aggregate)
+final_pred_error[,1]<-partysocv2.0eu_pred$aggregate
+final_pred_error[,2]<-sd_pred_model*t_value_MC
+final_pred_error[,3]<-sd_pred_model*t_value_BOOT
+final_pred_error[,4]<-partysocv2.0eu_pred$aggregate*re6_predictors$TOCre6
+final_pred_error[,5]<-(1-partysocv2.0eu_pred$aggregate)
+final_pred_error[,6]<-(1-partysocv2.0eu_pred$aggregate)*re6_predictors$TOCre6
 View(final_pred_error)
-
+# Save results as a csv file
 write.csv2(final_pred_error, "name_of_result_file.csv")
